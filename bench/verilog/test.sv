6aa8cb5cb6d9ac5aa5a6cdedd9bba722fc5575a7
Richard Herveille <richard.herveille@roalogic.com>
2017-09-04 16:40:33 +0200

    Added new logo

--- bench/verilog/test.sv
+++ bench/verilog/test.sv
@@ -1,46 +1,35 @@
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//    ██████╗  ██████╗  █████╗                                 //
-//    ██╔══██╗██╔═══██╗██╔══██╗                                //
-//    ██████╔╝██║   ██║███████║                                //
-//    ██╔══██╗██║   ██║██╔══██║                                //
-//    ██║  ██║╚██████╔╝██║  ██║                                //
-//    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝                                //
-//          ██╗      ██████╗  ██████╗ ██╗ ██████╗              //
-//          ██║     ██╔═══██╗██╔════╝ ██║██╔════╝              //
-//          ██║     ██║   ██║██║  ███╗██║██║                   //
-//          ██║     ██║   ██║██║   ██║██║██║                   //
-//          ███████╗╚██████╔╝╚██████╔╝██║╚██████╗              //
-//          ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝ ╚═════╝              //
-//                                                             //
-//    AHB3-Lite Time Testbench (Tests)                         //
-//                                                             //
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//     Copyright (C) 2017 ROA Logic BV                         //
-//     www.roalogic.com                                        //
-//                                                             //
-//    This source file may be used and distributed without     //
-//  restrictions, provided that this copyright statement is    //
-//  not removed from the file and that any derivative work     //
-//  contains the original copyright notice and the associated  //
-//  disclaimer.                                                //
-//                                                             //
-//    This soure file is free software; you can redistribute   //
-//  it and/or modify it under the terms of the GNU General     //
-//  Public License as published by the Free Software           //
-//  Foundation, either version 3 of the License, or (at your   //
-//  option) any later versions.                                //
-//  The current text of the License can be found at:           //
-//  http://www.gnu.org/licenses/gpl.html                       //
-//                                                             //
-//    This source file is distributed in the hope that it will //
-//  be useful, but WITHOUT ANY WARRANTY; without even the      //
-//  implied warranty of MERCHANTABILITY or FITTNESS FOR A      //
-//  PARTICULAR PURPOSE. See the GNU General Public License for //
-//  more details.                                              //
-//                                                             //
-/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//                                             `---'               //
+//   AHB3-Lite Timer Testbench (Tests)                             //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+//             Copyright (C) 2017 ROA Logic BV                     //
+//             www.roalogic.com                                    //
+//                                                                 //
+//   This source file may be used and distributed without          //
+//   restriction provided that this copyright statement is not     //
+//   removed from the file and that any derivative work contains   //
+//   the original copyright notice and the associated disclaimer.  //
+//                                                                 //
+//     This soure file is free software; you can redistribute it   //
+//   and/or modify it under the terms of the GNU General Public    //
+//   License as published by the Free Software Foundation, either  //
+//   version 3 of the License, or (at your option) any later       //
+//   versions. The current text of the License can be found at:    //
+//   http://www.gnu.org/licenses/gpl.html                          //
+//                                                                 //
+//    This source file is distributed in the hope that it will be  //
+//  useful, but WITHOUT ANY WARRANTY; without even the implied     //
+//  warranty of MERCHANTABILITY or FITTNESS FOR A PARTICULAR       //
+//  PURPOSE. See the GNU General Public License for more details.  //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
 
 module test #(
   parameter TIMERS = 2,       //Number of timers
